ad395e092840f7c3b57a621760e206a4170b0870
Surbhi <surbhikumaridav@gmail.com>
2023-06-22 19:19:12 +0530

    update notification server

--- index.js
+++ index.js
@@ -11,41 +11,81 @@ const tokens = [];
 
 app.post("/register", (req, res) => {
   const token = req.headers.authorization;
+console.log(token)
+
+  // Check if the token already exists in the database
   const tokensRef = admin.database().ref("fcmTokens");
   tokensRef
-    .push({ token })
-    .then(() => {
-      res.status(200).json({ message: "Token stored successfully." });
+    .orderByChild("token")
+    .equalTo(token)
+    .once("value")
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        // Token already exists, return an error
+        console.log("Token already exist")
+        res.status(200).json({ error: "Token already exists." });
+
+      } else {
+        // Token is unique, store it in the database
+        tokensRef
+          .push({ token })
+          .then(() => {
+            res.status(200).json({ message: "Token stored successfully." });
+          })
+          .catch((error) => {
+            res.status(500).json({ error: "Failed to store token." });
+          });
+      }
     })
     .catch((error) => {
-      res.status(500).json({ error: "Failed to store token." });
+      res.status(500).json({ error: "Failed to check token uniqueness." });
     });
 });
+
 
 app.post("/push", (req, res) => {
-  const token = req.headers.authorization;
-  console.log(token);
+  const message = req.body;
 
-  const message = {
-    data: {
-      dodsfvv: "fhofao",
-    },
-    token: token,
-  };
+  const tokensRef = admin.database().ref("fcmTokens");
+  tokensRef.once("value")
+    .then((snapshot) => {
+      const tokens = [];
+      snapshot.forEach((childSnapshot) => {
+        const token = childSnapshot.val().token;
+        tokens.push(token);
+      });
 
-  admin
-    .messaging()
-    .send(message)
-    .then((response) => {
-      console.log(response);
-      //configure your response here
+      if (tokens.length === 0) {
+        res.status(400).json({ error: "No tokens found." });
+        return;
+      }
 
-      res.status(201).send("Notification sent successfully");
+      const payload = {
+        notification: message.notification,
+        data: message.data,
+      };
+
+      const multicastMessage = {
+        tokens: tokens,
+        ...payload,
+      };
+
+      admin.messaging().sendEachForMulticast(multicastMessage)
+        .then((response) => {
+          console.log("Successfully sent notifications:", response);
+          res.status(201).json({ message: "Notification sent successfully." });
+        })
+        .catch((error) => {
+          console.error("Error sending notifications:", error);
+          res.status(500).json({ error: "Failed to send notifications." });
+        });
     })
     .catch((error) => {
-      console.log(error);
+      console.error("Error retrieving tokens:", error);
+      res.status(500).json({ error: "Failed to retrieve tokens." });
     });
 });
+
 app.listen(port, () => {
-  console.log("listening to port" + port);
+  console.log("Listening on port " + port);
 });
